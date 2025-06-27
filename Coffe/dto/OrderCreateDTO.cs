d068df5261df6d085dd1236b6a0d528f15d84b86
GhostDragon <bk.if3@ya.ru>
2025-06-27 18:27:04 +0400

    adding dto

---
+++ Coffe/dto/OrderCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using Coffe.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffe.dto
+{
+    internal class OrderCreateDTO
+    {
+        public string CustomerName { get; set; } = "";
+        public List<Drink> Drinks { get; set; } = new();
+    }
+}
