044434d578490bc3071628a4ee859d5823832c28
GhostDragon <bk.if3@ya.ru>
2025-06-27 18:44:17 +0400

    Create DiscountServiceImpl.cs

---
+++ Coffe/service/Impl/DiscountServiceImpl.cs
@@ -0,0 +1,30 @@
+﻿using Coffe.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffe.service.Impl
+{
+    internal class DiscountServiceImpl : IDiscountService
+    {
+        public double ApplyDiscount(double amount, DateTime time)
+        {
+            TimeOfDay timeOfDay = time.Hour switch
+            {
+                >= 6 and < 12 => TimeOfDay.Morning,
+                >= 12 and < 18 => TimeOfDay.Afternoon,
+                >= 18 and < 23 => TimeOfDay.Evening,
+                _ => TimeOfDay.Night
+            };
+
+            return timeOfDay switch
+            {
+                TimeOfDay.Morning => amount * 0.9,     // 10% утренняя скидка
+                TimeOfDay.Evening => amount * 0.95,    // 5% вечерняя
+                _ => amount
+            };
+        }
+    }
+}
