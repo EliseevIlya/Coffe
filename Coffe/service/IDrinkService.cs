9dce7835280457ff57558ade160f140e77ec555f
GhostDragon <bk.if3@ya.ru>
2025-06-27 15:07:47 +0400

    Update IDrinkService.cs

--- Coffe/service/IDrinkService.cs
+++ Coffe/service/IDrinkService.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Coffe.entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Coffe.entity.service
+namespace Coffe.service
 {
     internal interface IDrinkService
     {
+        void AddDrink(Drink drink);
+        void UpdateDrink(Drink drink);
+        void DeleteDrink(Drink drink);
+        List<Drink> GetAllDrinks();
+        Drink GetDrink(int id);
+
     }
 }
