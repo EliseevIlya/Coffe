251ae46800e41d37df0c810f02223b90bf035016
GhostDragon <bk.if3@ya.ru>
2025-06-27 15:37:59 +0400

    Create DrinkServiceImpl.cs

---
+++ Coffe/service/Impl/DrinkServiceImpl.cs
@@ -0,0 +1,44 @@
+﻿using Coffe.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffe.service.Impl
+{
+    internal class DrinkServiceImpl : IDrinkService
+    {
+        private readonly IRepository<Drink> _repository;
+
+        public DrinkServiceImpl(IRepository<Drink> repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddDrink(Drink drink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDrink(Drink drink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Drink> GetAllDrinks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Drink GetDrink(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDrink(Drink drink)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
