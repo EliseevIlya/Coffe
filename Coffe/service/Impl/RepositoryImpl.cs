f84e5a5e2f84f456f91f00fe2185dfeb3a36dc5a
GhostDragon <bk.if3@ya.ru>
2025-06-27 14:56:26 +0400

    create Reposetory

---
+++ Coffe/service/Impl/RepositoryImpl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffe.service.Impl
+{
+    internal class RepositoryImpl<T> : IRepository<T> where T : class
+    {
+        private readonly List<T> Items = new();
+        public void Add(T entity)
+        {
+            
+        }
+
+        public void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
