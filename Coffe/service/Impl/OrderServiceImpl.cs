320329d49480bd2674ad49e56c0d03a98cdb7d70
GhostDragon <bk.if3@ya.ru>
2025-06-27 18:48:32 +0400

    Update OrderServiceImpl.cs

--- Coffe/service/Impl/OrderServiceImpl.cs
+++ Coffe/service/Impl/OrderServiceImpl.cs
@@ -11,10 +11,12 @@ namespace Coffe.service.Impl
     internal class OrderServiceImpl : IOrderService
     {
         private readonly IRepository<Order> _repository;
+        private readonly DiscountServiceImpl _discountService;
 
-        public OrderServiceImpl(IRepository<Order> repository)
+        public OrderServiceImpl(IRepository<Order> repository, DiscountServiceImpl discountService)
         {
             _repository = repository;
+            _discountService = discountService;
         }
 
         public void DeleteOrder(int id)
@@ -37,7 +39,9 @@ namespace Coffe.service.Impl
             var order = new Order();
             order.Drinks = createDTO.Drinks;
             order.CustomerName = createDTO.CustomerName;
-            order.OrderPrice = createDTO.Drinks.Sum(x => x.Price);
+            double totalPrice = createDTO.Drinks.Sum(x => x.Price);
+            double finalPrice = _discountService.ApplyDiscount(totalPrice, DateTime.Now);
+            order.OrderPrice = finalPrice;
             _repository.Add(order);
         }
     }
