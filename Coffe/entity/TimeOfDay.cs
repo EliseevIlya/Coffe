81342b39efdbc2fd08ce46a8f31c9d29602fcc7b
GhostDragon <bk.if3@ya.ru>
2025-06-27 18:44:05 +0400

    Create TimeOfDay.cs

---
+++ Coffe/entity/TimeOfDay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffe.entity
+{
+    internal enum TimeOfDay
+    {
+        Morning,
+        Afternoon,
+        Evening,
+        Night
+
+    }
+}
